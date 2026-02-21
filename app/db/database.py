7f55d0f263b2d4f65b3091b440cb7a76442747e2
srigayatrii <sriigayatri06@gmail.com>
2026-02-22 00:11:26 +0530

    database.py setup with .env

---
+++ app/db/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    echo = True
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+base=declarative_base()
