6783ec8d8a650e75b501994a0e910564abf1794e
srigayatrii <sriigayatri06@gmail.com>
2026-02-22 18:26:28 +0530

    user and detection schema

---
+++ app/schemas/detection.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Dict, Any
+
+class DetectionRequestCreate(BaseModel):
+    text: str
+    model_used: str
+
+class DetectionRequestResponse(BaseModel):
+    id: int
+    user_id: int
+    text: str
+    input_length: int
+    model_used: str
+    status: str
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+class DetectionResultResponse(BaseModel):
+    id: int
+    request_id: int
+    ai_probability: float
+    metrics: Dict[str, Any]
+    created_at: datetime
+
+    class config:
+        from_attributes = True
