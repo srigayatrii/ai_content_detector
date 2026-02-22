68e4a68e7be913b9645eaef3a3ca0b870caf534e
srigayatrii <sriigayatri06@gmail.com>
2026-02-22 12:44:03 +0530

    user.py setup

---
+++ app/db/models/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, DateTime, String
+from datetime import datetime
+from app.db.database import base
+
+class User(base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, nullable=False, index=True)
+    password_hash = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    
--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from app.db.database import engine
+
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "ai content detector is working"}
+
+@app.get("/test_db")
+def test_db():
+    try:
+        with engine.connect() as connection:
+            return { "status" : "connected successfully"}
+    except Exception as e:
+        return {"error", str(e)}
